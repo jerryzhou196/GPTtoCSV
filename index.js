3cc0d6b4a8a202a6eea8db49c9826f00d88e762e
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2022-12-10 23:46:08 -0500

    implenting csv download system

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log("PLEASE WORK")
+
+// Collect the data that you want to save to the CSV file
+const data = [
+];
+
+
+
+
 
 function handleChange(div) {
     var p = $('p', div);
@@ -7,13 +14,41 @@ function handleChange(div) {
         console.log("PROMPT: " + div.innerHTML)
     } else {
         if (p.toArray().length > 1) {
-            p.toArray().forEach((pp) => (console.log("RESPONSE: " + pp.innerHTML)));
+            p.toArray().forEach((pp) => (writeToFile("RESPONSE: " + pp.innerHTML)));
         } else {
-            console.log("RESPONSE: " + p.html());
+            writeToFile("RESPONSE: " + p.innerHTML)
         }
     }
 }
 
+
+
+function writeToFile(stringToWrite, type) {
+    data[type] = stringToWrite
+
+}
+
+
+
+function exportData() {
+    // Convert the data to a string in JSON format
+    const jsonData = JSON.stringify(data);
+
+    // Encode the JSON string as base64
+    const base64Data = btoa(jsonData);
+
+    // Create a URL that can be used to download the CSV file
+    const csvUrl = `data:text/csv;base64,${base64Data}`;
+
+    // Create a link that will trigger the download when clicked
+    const link = document.createElement("a");
+    link.setAttribute("href", csvUrl);
+    link.setAttribute("download", "data.csv");
+    link.innerText = "Download CSV";
+
+    // Append the link to the page
+    document.body.appendChild(link);
+}
 
 
 function exportGPT() {
@@ -21,4 +56,6 @@ function exportGPT() {
         return $(this).hasClass('whitespace-pre-wrap') && $(this).hasClass('flex') && $(this).hasClass('flex-col') && $(this).hasClass('items-start')
     }).toArray().forEach((div) => (handleChange(div)))
 }
+
+
 
