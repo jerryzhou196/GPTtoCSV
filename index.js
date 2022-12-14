6bfb610d9becd189bd0084bc91cbbf9713acd495
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2022-12-13 21:18:06 -0500

    implenented papaparse

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
 
 // Collect the data that you want to save to the CSV file
-const data = [
+let data = [
 ];
 
-
-
-
+let temp;
+var prev;
 
 function handleChange(div) {
+
     var p = $('p', div);
 
-    if (p.toArray().length == 0) {
-        console.log("PROMPT: " + div.innerHTML)
+    if (count % 0) {
+        if (p.toArray().length == 0) {
+            prev = "div.innerHTML"
+            console.log("PROMPT: " + div.innerHTML)
+        }
     } else {
         if (p.toArray().length > 1) {
-            p.toArray().forEach((pp) => (writeToFile("RESPONSE: " + pp.innerHTML)));
+            p.toArray().forEach((pp) => (data.push({ "Question": pp.innerHTML, "Response": pp.innerHTML })));
         } else {
-            writeToFile("RESPONSE: " + p.innerHTML)
+            console.log("RESPONSE: " + p.html())
+            data.push({ "Question": prev, "Response": p.html() })
         }
+        prev = ""
+
     }
-}
-
-
-
-function writeToFile(stringToWrite, type) {
-    data[type] = stringToWrite
 
 }
-
 
 
 function exportData() {
-    // Convert the data to a string in JSON format
-    const jsonData = JSON.stringify(data);
+    // // Convert the data to a string in JSON format
+    // const jsonData = JSON.stringify(data);
 
-    // Encode the JSON string as base64
-    const base64Data = btoa(jsonData);
+    // // Encode the JSON string as base64
+    // const base64Data = btoa(jsonData);
 
-    // Create a URL that can be used to download the CSV file
-    const csvUrl = `data:text/csv;base64,${base64Data}`;
+    // // Create a URL that can be used to download the CSV file
+    // const csvUrl = `data:text/csv;base64,${base64Data}`;
 
-    // Create a link that will trigger the download when clicked
-    const link = document.createElement("a");
-    link.setAttribute("href", csvUrl);
-    link.setAttribute("download", "data.csv");
-    link.innerText = "Download CSV";
+    // // Create a link that will trigger the download when clicked
+    // const link = document.createElement("a");
+    // link.setAttribute("href", csvUrl);
+    // link.setAttribute("download", "data.csv");
+    // link.innerText = "Download CSV";
 
-    // Append the link to the page
-    document.body.appendChild(link);
+    // // push the link to the page
+    // document.body.pushChild(link);
 }
 
 
 function exportGPT() {
+    data = []
     $('div').filter(function () {
         return $(this).hasClass('whitespace-pre-wrap') && $(this).hasClass('flex') && $(this).hasClass('flex-col') && $(this).hasClass('items-start')
     }).toArray().forEach((div) => (handleChange(div)))
+
+    console.log(data)
+    let csv = Papa.unparse(data)
+    var csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+
+    console.log(Papa)
+    csvURL = window.URL.createObjectURL(csvData);
+
+    var tempLink = document.createElement('a');
+    tempLink.href = csvURL;
+    tempLink.setAttribute('download', 'download.csv');
+    tempLink.click();
+
+
 }
 
 
